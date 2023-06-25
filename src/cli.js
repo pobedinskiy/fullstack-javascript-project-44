e8ed9bc6d81b067766131c987dcd4f5e8afa3f83
pobedinskiy <dennis8860@gmail.com>
2023-06-25 16:32:35 +0300

    step 4

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,7 @@
 import readlineSync from 'readline-sync';
 
 const cli = () => {
-    const name = readlineSync.question('May I have your name? ');
-    console.log ('Hello, ' + name + '!');
-}
-
-export {cli};
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+};
+export default cli;
